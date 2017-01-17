ae9c55d0c150b774fae3168bcffda3aae65dd9d5
Akim Boyko <a.boyko@criteo.com>
2017-01-17 14:53:19 +0100

    Solve first 20 assignments including multiple solutions for `score` koans

--- python3/koans/about_scoring_project.py
+++ python3/koans/about_scoring_project.py
@@ -32,9 +32,74 @@ from runner.koan import *
 #
 # Your goal is to write the score method.
 
+from collections import Counter
+
+# my original
 def score(dice):
-    # You need to write this method
-    pass
+    total = 0
+    dice = Counter(dice)
+    while len(dice):
+        selected_index = next(filter(lambda n: dice[n] >= 3, [2, 3, 4, 5, 6]), None)
+        if selected_index:
+            total += selected_index * 100
+            dice[selected_index] -= 3
+        elif 1 in dice:
+            selected_index = 1
+            if dice[selected_index] >= 3:
+                total += 1000
+                dice[selected_index] -= 3
+            else:
+                total += 100
+                dice[selected_index] -= 1
+        elif 5 in dice:
+            total += 50
+            selected_index = 5
+            dice[selected_index] -= 1
+        #print(selected_index, dice)
+        if selected_index:
+            if dice[selected_index] == 0:
+                del dice[selected_index]
+        else:
+            break
+    return total
+
+# Michael Sanchez
+# def score(dice):
+#     # A dict to count the dice values
+#     d_dice = Counter(dice)
+#     # Score 1's and 5's
+#     score =   d_dice[1]//3 * 1000 \
+#             + d_dice[1]%3 * 100  \
+#             + d_dice[5]%3 * 50
+#     # Score triplets according to their value
+#     score += sum([d_dice[i]//3 * i * 100 for i in range(2,7)])
+#     return score
+
+# Rudy Sicard
+# def score(dice):
+#     score = 0
+#     counts = Counter(dice)
+#     _3_timers = set(v for v, count in counts.items() if count >= 3)
+#     for v in _3_timers:
+#         score += v * (1000 if v == 1 else 100)
+#     for v, bonus in [(1, 100), (5, 50)]:
+#         score += bonus * (counts[v] - (3 if v in _3_timers else 0))
+#     return score
+
+from collections import defaultdict, Counter
+
+# Sergei Lebedev
+# def score(dice):
+#     coef1 = defaultdict(int, {1: 100, 5: 50})
+#     coef3 = {1: 1000, 2: 200, 3: 300, 4: 400, 5: 500, 6: 600}
+#
+#     dice = Counter(dice)
+#     total = 0
+#     for n in range(1, 6 + 1):
+#         total += coef3[n] * (dice[n] // 3)
+#         total += coef1[n] * (dice[n] % 3)
+#
+#     return total
 
 class AboutScoringProject(Koan):
     def test_score_of_an_empty_list_is_zero(self):
